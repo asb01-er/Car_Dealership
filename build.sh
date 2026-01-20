082b515eec643c5b612d14737d92fc8154813738
asb01-er <ekelemernesto17@gmail.com>
2026-01-20 12:22:24 +0200

    Add requirements.txt and correct build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
-set -o errexit
+set -e
 
-pip install -r requirements.
+echo "==> Installing Python dependencies..."
+pip install --upgrade pip
+pip install -r requirements.txt   # <-- make sure this matches the actual filename
+
+echo "==> Running database migrations..."
 python manage.py migrate --noinput
+
+echo "==> Collecting static files..."
 python manage.py collectstatic --noinput
-python manage.py migrate
+
+echo "==> Build completed!"
